c9c3a52479910f8e8e1c39f9471510f5a48fcdc8
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2020-07-16 16:46:34 -0300

    Adicionando Tooltip de mensagem de erro de validação

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,8 @@
 import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback} from 'react'
 import {IconBaseProps} from 'react-icons'
+import {FiAlertCircle} from 'react-icons/fi'
 import { useField } from '@unform/core'
-import {Container} from './styles'
+import {Container, Errors} from './styles'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
   name: string;
@@ -13,7 +14,7 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
   const inputRef = useRef<HTMLInputElement>(null)
   const [isFocused, setIsFocused] = useState(false)
   const [isFilled,setIsFilled] = useState(false)
-  const {fieldName,defaultValue, registerField} = useField(name);
+  const {fieldName,defaultValue, error ,registerField} = useField(name);
 
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
@@ -35,9 +36,13 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
   },[fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused} isFilled={isFilled}>
+    <Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
       {Icon && <Icon size={20} />}
       <input onFocus={handleInputFocus} onBlur={handleInputBlur} defaultValue={defaultValue} ref={inputRef} {...rest} />
+      {error && 
+        <Errors title={error}>
+            <FiAlertCircle color="#c53030" size={20} />
+        </Errors>}
     </Container>
   );
 };
